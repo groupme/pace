648aef8ed01dd2f62d7ad982cf54471250ec1115
Dave Yeu <daveyeu@gmail.com>
2011-06-21 11:27:11 -0400

    Add test helper; version bump

---
+++ lib/pace/mock.rb
@@ -0,0 +1,65 @@
+# Ease testing by mocking the event loop
+#
+# Instead of having to detect and stop the event loop yourself, this helper
+# simply returns all jobs in the queue and shuts down the loop.
+#
+#     require "pace/mock"
+#
+#     # Fire it up
+#     Pace::Mock.enable
+#
+#     # Add some jobs
+#     Resque.enqueue(Work, ...)
+#     Resque.enqueue(Work, ...)
+#
+#     # Create a worker with a block that doesn't need to stop the loop
+#     worker = Pace::Worker.new(:queue => "queue")
+#     worker.start do |job|
+#       puts job.inspect
+#     end
+#
+#     # Turn it off when you're done
+#     Pace::Mock.disable
+#
+module Pace
+  module Mock
+    def self.enable
+      Pace.logger.info "Enabling Pace mock"
+
+      Pace::Worker.class_eval do
+        if instance_methods.include?(:start_with_mock)
+          alias :start :start_with_mock
+        else
+          def start_with_mock(&block)
+            jobs = nil
+
+            EM.run do
+              @redis = EM::Protocols::Redis.connect(@options)
+              @redis.lrange(queue_name, 0, -1) do |res|
+                jobs = res
+              end
+              @redis.del(queue_name) { EM.stop_event_loop }
+            end
+
+            jobs.each do |job|
+              block.call JSON.parse(job)
+            end
+          end
+
+          alias :start_without_mock :start
+          alias :start :start_with_mock
+        end
+      end
+    end
+
+    def self.disable
+      Pace.logger.info "Disabling Pace mock"
+
+      Pace::Worker.class_eval do
+        if instance_methods.include?(:start_without_mock)
+          alias :start :start_without_mock
+        end
+      end
+    end
+  end
+end
---
+++ spec/pace/mock_spec.rb
@@ -0,0 +1,72 @@
+require "spec_helper"
+require "pace/mock"
+
+describe Pace::Mock do
+  class Work
+    def self.queue
+      "pace"
+    end
+  end
+
+  before do
+    Resque.dequeue(Work)
+  end
+
+  after do
+    Pace::Mock.disable
+  end
+
+  describe ".enable" do
+    it "sets up the mock, which simply passes down Resque jobs and closes the event loop" do
+      Pace::Mock.enable
+      Resque.enqueue(Work, :n => 1)
+      Resque.enqueue(Work, :n => 2)
+
+      results = []
+      worker = Pace::Worker.new(:queue => "pace")
+      worker.start { |job| results << job }
+      results.should == [
+        {"class" => "Work", "args" => [{"n" => 1}]},
+        {"class" => "Work", "args" => [{"n" => 2}]}
+      ]
+
+      # Clears out the queue
+      more_results = []
+      worker.start { |job| more_results << job }
+      more_results.should be_empty
+    end
+
+    it "works after disabling" do
+      Pace::Mock.enable
+      Pace::Mock.disable
+      Pace::Mock.enable
+
+      Resque.enqueue(Work, :n => 2)
+
+      results = []
+      worker = Pace::Worker.new(:queue => "pace")
+      worker.start do |job|
+        results << job
+      end
+      results.should == [{"class" => "Work", "args" => [{"n" => 2}]}]
+    end
+  end
+
+  describe ".disable" do
+    it "tears down the mock and re-institutes the event loop" do
+      Pace::Mock.enable
+      Pace::Mock.disable
+      Resque.enqueue(Work, :n => 1)
+      Resque.enqueue(Work, :n => 2)
+
+      results = []
+      worker = Pace::Worker.new(:queue => "pace")
+      worker.start do |job|
+        results << job
+        EM.stop_event_loop
+      end
+
+      results.should have(1).items
+    end
+  end
+end
