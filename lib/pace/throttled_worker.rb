09a50071809464740530b43fada89aefaa38ab0a
Brandon Keene <bkeene@gmail.com>
2011-08-10 18:25:28 -0400

    Don't install timer until start

--- lib/pace/throttled_worker.rb
+++ lib/pace/throttled_worker.rb
@@ -26,14 +26,14 @@
 module Pace
   class ThrottledWorker < Worker
     def initialize(queue, limit, refresh_interval = 1)
+      @refresh_interval = refresh_interval
       @credits = @limit = limit
-
-      EM::add_periodic_timer(refresh_interval) do
-        EM.next_tick { fetch_next_job } if @credits < 1
-        @credits = @limit
-      end
-
       super(queue)
+    end
+
+    def start(&block)
+      install_refresh_timer
+      super
     end
 
     alias :fetch_next_job_without_throttling :fetch_next_job
@@ -42,6 +42,15 @@ module Pace
       return if @credits < 1
       @credits -= 1
       fetch_next_job_without_throttling
+    end
+
+    private
+
+    def install_refresh_timer
+      EM::add_periodic_timer(@refresh_interval) do
+        EM.next_tick { fetch_next_job } if @credits < 1
+        @credits = @limit
+      end
     end
   end
 end
